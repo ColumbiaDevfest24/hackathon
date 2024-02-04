7614d127ad93abb2dd30f2863dbffd323692516a
MS <151979600+mcserodio@users.noreply.github.com>
2024-02-03 22:55:27 -0500

    Added Organization class to models.py
    Attributes: name, website url, address, contact number, contact email, [opening] hours

--- main/models.py
+++ main/models.py
@@ -14,3 +14,23 @@
 
 #     def __str__(self):
 #         return self.user.username
+#class Organization(models.Model):
+    def __init__(self):
+        self._name=''
+        self._website_url=''
+        self._address=''
+        self._contact_num=''
+        self._contact_email=''
+        self._hours=''
+    def add_username(self,name):
+        self._name=name
+    def add_website_url(self,website_url):
+        self._website_url=website_url
+    def add_address(self,address):
+        self._address=address
+    def add_contact_num(self,contact_num):
+        self._contact_num=contact_num
+    def add_contact_email(self,contact_email):
+        self._contact_email=contact_email
+    def add_hours(self,hours):
+        self._hours=hours
